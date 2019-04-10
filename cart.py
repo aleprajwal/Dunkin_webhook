df3ee2df975cba76350041eab50059a7e2686eb1
Prajwal Ale <aleprajwal11@gmail.com>
2019-04-10 14:55:19 +0545

    initial commit

---
+++ cart.py
@@ -0,0 +1,52 @@
+class Drinks_Item(object):
+    def __init__(self, name, size, qty):
+        self.name = name
+        self.size = size
+        self.qty = qty
+
+
+class Bakery_Item(object):
+    def __init__(self, name, qty, *args):
+        self.name = name
+        self.qty = qty
+
+
+class Cart(object):
+    def __init__(self):
+        self.drinks_content = dict()    ## store details of ordered drinks items
+        self.bakery_content = dict()    ## store details of ordreed bakery items
+
+    ## update drinks order in cart
+    def drinks_update(self, item):
+        name = item.size + ' ' + item.name  ## creating key(name) with size and name of item
+        if name not in self.drinks_content: ## check if ordered drink item is perviously added
+            self.drinks_content.update({name: item})  ## adding drinks item in drinks_content dictonary with name as key
+            return
+        self.drinks_content.get(name).qty += item.qty ## if drinks is previously ordered, only increase its quantity
+
+    ## update bakery item order in cart
+    ## steps are similar to drinks_update()
+    def bakery_update(self, item):
+        name = item.name
+        if name not in self.bakery_content:
+            self.bakery_content.update({name: item})
+            return
+        self.bakery_content.get(name).qty += item.qty
+
+    ## show items available in cart
+    def show_items(self):
+        item_list = list()
+        for _, v in self.drinks_content.iteritems(): ## loop through items available in drinks_content
+            res = '{} {} {}'.format(v.qty, v.size, v.name)  ## creating a proper response eg. 2 large coffee
+            item_list.append(res)
+        for _, v in self.bakery_content.iteritems():
+            res = '{} {}'.format(v.qty, v.name) ## eg. 5 Donut
+            item_list.append(res)
+        return item_list    ## return list of ordered items
+
+    ## remove selected items from cart
+    def remove_item(self, key):
+        if key in self.drinks_content:
+            self.drinks_content.pop(key)
+        else:
+            self.bakery_content.pop(key)
---
+++ webhook.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, make_response
+import cart
+import logging
+import json
+import os
+
+
+# initilize flask app
+app = Flask(__name__)
+
+bag = cart.Cart()
+
+@app.route('/', methods=['POST', 'GET'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    try:
+        action = req.get('queryResult').get('action')
+    except AttributeError:
+        return 'JSON Error'
+
+    ## action to adds drinks item in cart
+    if action == 'order.items.drinks':
+        params = req.get('queryResult').get('parameters')
+        try:
+            for a,s,d in zip(params['number'],params['size'],params['drink']):
+                item = cart.Drinks_Item(d,s,int(a))
+                bag.drinks_update(item)
+
+            response = {'fulfillmentText':'Anything else?'}
+            res = json.dumps(response, indent=4)
+            r = make_response(res)
+            return r
+        except Exception as e:
+            logging.error('500 Error  --> order.items.drinks intent', exc_info=True)
+
+    ## action to adds bakery item in cart
+    if action == 'order.items.bakery':
+        params = req.get('queryResult').get('parameters')
+        try:
+            for n, b in zip(params['number'],params['bakery']):
+                item = cart.Bakery_Item(b, int(n))
+                bag.bakery_update(item)
+            response = {'fulfillmentText':'Anything else?'}
+            res = json.dumps(response, indent=4)
+            r = make_response(res)
+            return r
+        except Exception as e:
+            logging.error('500 Error  --> order.items.bakery intent', exc_info=True)
+
+    ## action to shows ordered item list available in cart
+    if action == 'order.items.check':
+        try:
+            if not bag.show_items():
+                response = {'fulfillmentText':'Your order list is empty. You can order drinks and bakery items.'}
+                res = json.dumps(response, indent=4)
+                r = make_response(res)
+                return r
+            else:
+                response = {'fulfillmentText':'Order List:\n{}\nWould you want to checkout?'.
+                    format(','.join(item for item in bag.show_items()))}
+                res = json.dumps(response, indent=4)
+                r = make_response(res)
+                return r
+        except Exception as e:
+            logging.error('500 Error --> order.product.check intent', exc_info=True)
+
+    ## action to remove selected item from cart
+    if action == 'order.items.remove':
+        params = req.get('queryResult').get('parameters')
+        try:
+            remove_item = params['bakery']
+            if params['drink']:
+                remove_item = params['size'] + ' ' + params['drink']
+            try:
+                bag.remove_item(remove_item)
+                if bag.show_items():
+                    response = {'fulfillmentText': '{} removed from your order list!!\nItems in your order list:\n{}'.
+                                format(remove_item, ','.join(item for item in bag.show_items()))}
+                else:
+                    response = {'fulfillmentText': '{} removed from your order list!!\n Your order list is empty. '
+                                                   'You can make an order for drinks and bakery items.'.format(remove_item)}
+            except Exception:
+                response = {'fulfillmentText':'{} not in your order list!!'.format(remove_item)}
+
+            res = json.dumps(response, indent=4)
+            r = make_response(res)
+            return r
+        except Exception as e:
+            logging.error('500 Error --> order.items.remove intent', exc_info=True)
+
+
+# run app
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port, host='0.0.0.0')
+
