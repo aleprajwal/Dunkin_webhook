6a62e4119b63da31f361ba71e6c56c700c1cc3bb
aleprajwal <aleprajwal11@gmail.com>
2019-04-24 16:32:23 +0545

    response

---
+++ response.py
@@ -0,0 +1,3 @@
+COOL_WEATHER = '{temp} degrees!! Weather looks cool today, We have Hot Chocolate, Latte, Coffee and Tea. Make your order'
+
+SUNNY_WEATHER = '{temp} degrees!! Weather looks sunny today, We have Coke, Fanta and Pepsi. Make your order'
