aeeca49bb6b6e1e0e1448d166cf04ebf01a1c743
aleprajwal <aleprajwal11@gmail.com>
2019-04-24 14:23:54 +0545

    weather info

--- weather_api.py
+++ weather_api.py
@@ -4,19 +4,21 @@ import json
 import os
 
 host = ''
-wwoApiKey = ''
+wwoApiKey = '7ce567a627504e3c82951314192404'
 city = 'kathmandu'
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def weather():
+def weather_api():
     req = request.get_json(silent=True, force=True)
     try:
         action = req.get('queryResult').get('action')
     except AttributeError:
         return 'JSON Error'
 
+def weather_info():
+    path = '{}/premium/v1/weather.ashx?format=json&num_of_days=1&key={}&q={}'.format(host, wwoApiKey, city)
 
 # run app
 if __name__ == '__main__':
